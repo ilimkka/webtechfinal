86f40f53bd46ded4c81db7fa520cbe7f3f87fcb9
ilimkka <124762449+ilimkka@users.noreply.github.com>
2023-05-06 23:07:35 +0600

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,24 @@
+function addToCart(prod) {
+    let cart = getCart()
+    if (!cart.includes(prod)) {
+        cart.push(prod)
+        localStorage.setItem('cart', JSON.stringify(cart))
+    } else {
+        console.log('areadey exists');
+    }
+}
+
+function getCart() {
+    if (localStorage.getItem('cart') != null) {
+        return JSON.parse(localStorage.getItem('cart')) 
+    } else {
+        localStorage.setItem('cart', JSON.stringify([]))
+        return getCart()
+    }
+}
+
+function destroyItem(prod) {
+    if (getCart().includes(prod)) {
+        localStorage.setItem('cart', JSON.stringify(getCart().filter(i => i != prod)))
+    }
+}
